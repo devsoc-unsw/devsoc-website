fab71ee81a8779347c40935a5ef81b004a9063f3
Franco Reyes <franco.javier.reyes@gmail.com>
2023-10-25 01:38:06 +1100

    Create a box to put shit in

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import React from 'react';
 import NavBar from '../../components/nav/NavBar';
 import MobileNavBar from '../../components/nav/MobileNavBar';
+import { Sheet } from '@mui/joy';
 
 
 const Layout: React.FC<{
@@ -10,7 +13,19 @@ const Layout: React.FC<{
     <>
       <NavBar/>
       <MobileNavBar/>
-      {children}
+      <Sheet
+        variant='soft'
+        sx={{
+          mx: 'auto',
+          maxWidth: 900,
+          my: { md: 3 },
+          height: 1000, // TODO: Remove this when we have actual content
+          borderRadius: { md: '2%' },
+          p: 4
+        }}
+      >
+        {children}
+      </Sheet>
     </>
   )
 }
