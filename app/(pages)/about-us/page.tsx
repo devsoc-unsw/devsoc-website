11c6f6ea05ea6b31cc86e44a8d424e02f20ec0ea
Franco Reyes <franco.javier.reyes@gmail.com>
2023-10-25 02:16:30 +1100

    Basic page section structure

--- app/(pages)/about-us/page.tsx
+++ app/(pages)/about-us/page.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from '@mui/joy';
+import PageSection from '../../../components/PageSection';
 
 export default function AboutUsPage() {
   return (
     <div>
-      TODO: About us, objectives, constitution, meet the team
+      {/*TODO: About us, objectives, constitution, meet the team*/}
+      <PageSection title="Who We Are">
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+      </PageSection>
+      <PageSection title="Our Mission">
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+      </PageSection>
+      <PageSection title="Our Constitution">
+        You can find our constitution <Link>here</Link>.
+      </PageSection>
+      <PageSection title="Meet The Team">
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+        Dev is really cool, we have a lot of cool people and we do a lot of cool stuff.
+      </PageSection>
     </div>
   )
 }
+
---
+++ components/PageSection.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Divider, Typography } from '@mui/material';
+
+interface SectionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const PageSection: React.FC<SectionProps> = ({ title, children }) => {
+  return (
+    <Box mb={3}>
+      <Divider sx={{}}>
+        <Typography level="h1">{title}</Typography>
+      </Divider>
+      <Box sx={{ p: 1 }}>
+        {children}
+      </Box>
+    </Box>
+  )
+};
+
+export default PageSection;
