088626cedf642b0de0fa1fd217ad8e9e4efb65c9
Bob Chen <main.bobchen@gmail.com>
2024-09-27 17:39:41 +1000

    feature(content): use character for carousel button

--- components/embla/EmblaCarouselArrowButtons.tsx
+++ components/embla/EmblaCarouselArrowButtons.tsx
@@ -63,10 +63,7 @@ export const PrevButton: React.FC<PropType> = (props) => {
       type="button"
       {...restProps}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF"
-           className="embla__button__svg">
-        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"/>
-      </svg>
+      ▲
 
       {children}
     </button>
@@ -82,10 +79,7 @@ export const NextButton: React.FC<PropType> = (props) => {
       type="button"
       {...restProps}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF"
-           className="embla__button__svg">
-        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"/>
-      </svg>
+      ▼
 
       {children}
     </button>
