3a32c559ebb6a3e42722dbc9df2f3699566c3a5d
Bob Chen <main.bobchen@gmail.com>
2024-09-27 17:55:49 +1000

    feature(content): button test

--- components/embla/EmblaCarouselArrowButtons.tsx
+++ components/embla/EmblaCarouselArrowButtons.tsx
@@ -63,9 +63,10 @@ export const PrevButton: React.FC<PropType> = (props) => {
       type="button"
       {...restProps}
     >
-      ▲
-
-      {children}
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF"
+           className="embla__button__svg">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"/>
+      </svg>
     </button>
   )
 }
@@ -79,9 +80,10 @@ export const NextButton: React.FC<PropType> = (props) => {
       type="button"
       {...restProps}
     >
-      ▼
-
-      {children}
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF"
+           className="embla__button__svg">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"/>
+      </svg>
     </button>
   )
 }
