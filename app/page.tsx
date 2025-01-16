6e024116ae9b5933789d3a67d79cdb8aed0a520b
Dylan <redman29h@gmail.com>
2025-01-16 23:02:40 +1100

    Picture contrast and breakpoint adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
-'use client'
-import { AspectRatio, Card, Stack, Typography, useTheme } from '@mui/joy';
-import React, { useEffect, useRef } from 'react';
+"use client";
+import { AspectRatio, Card, Stack, Typography, useTheme } from "@mui/joy";
+import React, { useEffect, useRef } from "react";
 import { Box, Button, Link } from "@mui/material";
-import { projectData } from '../data';
+import { projectData } from "../data";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import { ProjectCardProps } from "../components/ProjectCard";
 import Image from "next/image";
-import { NextButton, PrevButton, usePrevNextButtons } from "../components/embla/EmblaCarouselArrowButtons";
-import "./styles.css"
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from "../components/embla/EmblaCarouselArrowButtons";
+import "./styles.css";
 import { useMediaQuery } from "@mui/system";
-import { CustomCarousel } from '../components/carousel/CustomCarousel';
+import { CustomCarousel } from "../components/carousel/CustomCarousel";
 
 interface EventCardProp {
   title: string;
@@ -26,54 +30,62 @@ const eventContent: EventCardProp[] = [
     href: "https://www.facebook.com/events/1586760535262553/",
     dateTime: "12-2PM October 22nd 2024",
     thumbnail: "",
-    button: "View event"
+    button: "View event",
   },
   {
     title: "Techspire",
     href: "https://techspire.devsoc.app/",
     dateTime: "3PM November 5th 2024",
     thumbnail: "",
-    button: "Find out more"
+    button: "Find out more",
   },
-]
+];
 
 export default function Home() {
   const scrollRef = useRef<HTMLDivElement | null>(null);
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, axis: 'y', duration: 45 }, [Autoplay({delay: 5000})]);
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { loop: true, axis: "y", duration: 45 },
+    [Autoplay({ delay: 5000 })]
+  );
   const {
     prevBtnDisabled,
     nextBtnDisabled,
     onPrevButtonClick,
-    onNextButtonClick
-  } = usePrevNextButtons(emblaApi)
-  const displayProjects = projectData.filter(x => x.name !== "Chaos" && x.name !== "Jobsboard")
-
-  useEffect(() => {
-    const handleScroll = (e: { deltaY: any; }) => {
-      if (scrollRef.current) {
-        // Adjust the scroll position of the div based on the window's scroll position
-        scrollRef.current.scrollLeft += e.deltaY; // or adjust based on scroll direction
-      }
-    };
-
-    window.addEventListener('wheel', handleScroll, { passive: false });
-
-    return () => {
-      window.removeEventListener('wheel', handleScroll);
-    };
-  }, []);
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+  const displayProjects = projectData.filter(
+    (x) => x.name !== "Chaos" && x.name !== "Jobsboard"
+  );
 
   return (
     <Box className="homeContent">
-      <Stack direction="column" pt={2} px={4} width="100%" maxWidth={1300} m="auto" overflow="hidden">
+      <Stack
+        direction="column"
+        pt={2}
+        px={4}
+        width="100%"
+        maxWidth={1300}
+        m="auto"
+        overflow="hidden"
+      >
         <Stack direction="column">
-          <Box sx={{width: "100%"}}>
-            <Typography className={"heroText"} mt={3} fontSize={{xs: "2rem", sm: "2.3rem", md: "3.2rem"}}
-                        fontWeight={500} component='div'>
+          <Box sx={{ width: "100%" }}>
+            <Typography
+              className={"heroText"}
+              mt={3}
+              fontSize={{ xs: "2rem", sm: "2.3rem", md: "3.2rem" }}
+              fontWeight={500}
+              component="div"
+            >
               UNSW&apos;s software development society
             </Typography>
-            <Typography className={"heroText"} mt={0} fontSize={{xs: "1.5rem", sm: "2rem", md: "2.5rem"}} fontWeight={200}
-                        component='div'>
+            <Typography
+              className={"heroText"}
+              mt={0}
+              fontSize={{ xs: "1.5rem", sm: "2rem", md: "2.5rem" }}
+              fontWeight={200}
+              component="div"
+            >
               Run by devs, for devs
             </Typography>
           </Box>
@@ -88,69 +100,70 @@ export default function Home() {
             Highlights
           </Typography>
         </Box> */}
-        <CustomCarousel/>
+        <CustomCarousel />
         <Box
           ref={scrollRef}
-          marginBottom={{xs: "3rem", md: 0}}
-          marginTop={{xs: "2rem", md: "3rem"}}
+          marginBottom={{ xs: "3rem", md: 0 }}
+          marginTop={{ xs: "2rem", md: "2rem" }}
           sx={{
-            display: 'flex',
-            overflowX: 'auto',
-            scrollbarWidth: 'none',
+            display: "flex",
+            overflowX: "auto",
+            scrollbarWidth: "none",
           }}
         >
           <Stack
-            spacing={{xs: 3, md: 5}}
-            direction={{xs: "column", md: "row"}}
-            sx={{display: "flex", width: '100%'}}
-            flexDirection={{xs: "column", md: "row"}}
+            spacing={{ xs: 3, lg: 5 }}
+            direction={{ xs: "column", lg: "row" }}
+            sx={{ display: "flex", width: "100%" }}
+            flexDirection={{ xs: "column", lg: "row" }}
           >
-            {displayProjects.map((props, idx) => <LinkCard key={props.name} {...props} order={idx}/>)}
+            {displayProjects.map((props, idx) => (
+              <LinkCard key={props.name} {...props} order={idx} />
+            ))}
           </Stack>
         </Box>
       </Stack>
     </Box>
-  )
+  );
 }
-
 
 const Slide: React.FC<EventCardProp> = ({
   title,
   href,
   dateTime,
   thumbnail,
-  button
+  button,
 }) => {
   let color = "#111111";
   const theme = useTheme();
-  const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
+  const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
   return (
     <Card
       className="embla__slide"
       sx={{
-        height: {xs: "200px", md: "500px"},
+        height: { xs: "200px", md: "500px" },
         backgroundColor: color,
         borderRadius: 0,
-        padding: thumbnail && isMdUp ? '2rem 1rem' : {xs: '1rem', md: '2rem'},
-        flexDirection: 'row',
-        gap: '4rem',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        border: 'none'
+        padding: thumbnail && isMdUp ? "2rem 1rem" : { xs: "1rem", md: "2rem" },
+        flexDirection: "row",
+        gap: "4rem",
+        alignItems: "center",
+        justifyContent: "space-between",
+        border: "none",
       }}
     >
-      <Box sx={{display: "flex", flexDirection: "row", gap: "2rem"}}>
-        {
-          thumbnail && isMdUp ? <Image width={170} height={90} src={thumbnail} alt={title}/> : null
-        }
-        <Box sx={{display: "flex", flexDirection: "column"}}>
+      <Box sx={{ display: "flex", flexDirection: "row", gap: "2rem" }}>
+        {thumbnail && isMdUp ? (
+          <Image width={170} height={90} src={thumbnail} alt={title} />
+        ) : null}
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
           <Typography
             sx={{
               color: "#EDEEF0",
-              fontSize: { xs: '1.5rem', sm: '1.75rem', md: '2rem' },
+              fontSize: { xs: "1.5rem", sm: "1.75rem", md: "2rem" },
               fontWeight: 500,
               alignSelf: "flex-start",
-              justifySelf: "center"
+              justifySelf: "center",
             }}
           >
             {title}
@@ -159,61 +172,59 @@ const Slide: React.FC<EventCardProp> = ({
             sx={{
               color: "#EDEEF0",
               alignSelf: "flex-start",
-              justifySelf: "center"
+              justifySelf: "center",
             }}
           >
             {dateTime}
           </Typography>
         </Box>
       </Box>
-      {
-        button ?
-          <Link href={href} sx={{ textDecoration: 'none' }}>
-            <Button variant="soft">{button}</Button>
-          </Link> : null
-      }
+      {button ? (
+        <Link href={href} sx={{ textDecoration: "none" }}>
+          <Button variant="soft">{button}</Button>
+        </Link>
+      ) : null}
     </Card>
-  )
-}
-
+  );
+};
 
 const HighlightLinkCard: React.FC<EventCardProp> = ({
   title,
   href,
   dateTime,
   thumbnail,
-  button
+  button,
 }) => {
   let color = "#111111";
   const theme = useTheme();
-  const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
+  const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
   return (
     <Card
       className="embla__slide"
       sx={{
-        height: {xs: "90px", md: "100px"},
+        height: { xs: "90px", md: "100px" },
         backgroundColor: color,
         borderRadius: 0,
-        padding: thumbnail && isMdUp ? '2rem 1rem' : {xs: '1rem', md: '2rem'},
-        flexDirection: 'row',
-        gap: '4rem',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        border: 'none'
+        padding: thumbnail && isMdUp ? "2rem 1rem" : { xs: "1rem", md: "2rem" },
+        flexDirection: "row",
+        gap: "4rem",
+        alignItems: "center",
+        justifyContent: "space-between",
+        border: "none",
       }}
     >
-      <Box sx={{display: "flex", flexDirection: "row", gap: "2rem"}}>
-        {
-          thumbnail && isMdUp ? <Image width={170} height={90} src={thumbnail} alt={title}/> : null
-        }
-        <Box sx={{display: "flex", flexDirection: "column"}}>
+      <Box sx={{ display: "flex", flexDirection: "row", gap: "2rem" }}>
+        {thumbnail && isMdUp ? (
+          <Image width={170} height={90} src={thumbnail} alt={title} />
+        ) : null}
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
           <Typography
             sx={{
               color: "#EDEEF0",
-              fontSize: { xs: '1.5rem', sm: '1.75rem', md: '2rem' },
+              fontSize: { xs: "1.5rem", sm: "1.75rem", md: "2rem" },
               fontWeight: 500,
               alignSelf: "flex-start",
-              justifySelf: "center"
+              justifySelf: "center",
             }}
           >
             {title}
@@ -222,47 +233,48 @@ const HighlightLinkCard: React.FC<EventCardProp> = ({
             sx={{
               color: "#EDEEF0",
               alignSelf: "flex-start",
-              justifySelf: "center"
+              justifySelf: "center",
             }}
           >
             {dateTime}
           </Typography>
         </Box>
       </Box>
-      {
-        button ?
-          <Link href={href} sx={{ textDecoration: 'none' }}>
-            <Button variant="soft">{button}</Button>
-          </Link> : null
-      }
+      {button ? (
+        <Link href={href} sx={{ textDecoration: "none" }}>
+          <Button variant="soft">{button}</Button>
+        </Link>
+      ) : null}
     </Card>
-  )
-}
+  );
+};
 
-const LinkCard: React.FC<Omit<ProjectCardProps, "trainee"> & { order: number }> = ({
-  name,
-  desc,
-  logoUrl,
-  thumbnailUrl,
-  projectUrl,
-  status,
-  order
-}) => {
+const LinkCard: React.FC<
+  Omit<ProjectCardProps, "trainee"> & { order: number }
+> = ({ name, desc, logoUrl, thumbnailUrl, projectUrl, status, order }) => {
   // Class 3 colors sourced from https://www.radix-ui.com/colors
-  const colors = ["#182449", "#0D2847", "#202248", "#351A35", "#391714", "#291F43", "#222222"]
+  const colors = [
+    "#182449",
+    "#0D2847",
+    "#202248",
+    "#351A35",
+    "#391714",
+    "#291F43",
+    "#222222",
+  ];
   return (
     <Link href={projectUrl}>
       <Card
         className="boxBackground"
         sx={{
-          width: { xs: "100%", md: "250px" },
-          height: { xs: "5rem", md: "100px" },
+          width: { xs: "100%", lg: "215px" },
+          height: { xs: "5rem", lg: "86px" },
           backgroundColor: colors[order],
-          boxShadow: 'sm',
-          padding: '1rem',
-          flexDirection: 'row',
-          gap: '2rem',
-          alignItems: 'center',
+          boxShadow: "sm",
+          padding: "1rem",
+          flexDirection: "row",
+          gap: "1.25rem",
+          alignItems: "center",
         }}
       >
         <AspectRatio objectFit="contain" ratio={1} sx={{ width: "3rem" }}>
@@ -276,7 +288,7 @@ const LinkCard: React.FC<Omit<ProjectCardProps, "trainee"> & { order: number }> 
         <Typography
           sx={{
             color: "#EDEEF0",
-            fontSize: {xs: '1.5rem', sm: '1.75rem', md: '1.5rem'},
+            fontSize: { xs: "1.5rem", lg: "1.25rem" },
             fontWeight: 500,
           }}
         >
@@ -284,6 +296,5 @@ const LinkCard: React.FC<Omit<ProjectCardProps, "trainee"> & { order: number }> 
         </Typography>
       </Card>
     </Link>
-  )
-}
-
+  );
+};
--- components/carousel/CustomCarousel.tsx
+++ components/carousel/CustomCarousel.tsx
@@ -1,51 +1,73 @@
-import React, { useEffect } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-import './embla.css';
-import { NextButton, PrevButton, usePrevNextButtons } from '../embla/EmblaCarouselArrowButtons';
+import React, { useEffect } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import "./embla.css";
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from "../embla/EmblaCarouselArrowButtons";
+import { Box, Typography } from "@mui/joy";
 
 export function CustomCarousel() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, watchDrag: false }, [Autoplay({delay: 5000})])
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { loop: true, watchDrag: false },
+    [Autoplay({ delay: 5000 })]
+  );
   const slides = [
-    {src: "/carousel/brainstorm.png", text: "Solve Unique Problems"},
-    {src: "/carousel/starlight.jpg", text: "Showcase your projects"},
-    {src: "/carousel/qa_panel.jpg", text: "Industry Events"},
-    {src: "/carousel/workshops.png", text: "Learn Together"},
-    {src: "/carousel/together.jpg", text: "Develop Together"},
-    {src: "/carousel/projects.png", text: "Our Projects"},
-    {src: "/projects/notangles/thumbnail.png", text: "Notangles"},
-    {src: "/projects/freerooms/thumbnail.png", text: "Freerooms"},
-    {src: "/projects/unilectives/thumbnail.png", text: "Unilectives"},
-    {src: "/projects/circles/thumbnail.png", text: "Circles"},
-    {src: "/projects/structs/thumbnail.png", text: "Structs.sh"},
-    {src: "/carousel/applications.png", text: "Join us!"},
-  ]
+    { src: "/carousel/brainstorm.png", text: "Solve Unique Problems" },
+    { src: "/carousel/starlight.jpg", text: "Showcase your projects" },
+    { src: "/carousel/qa_panel.jpg", text: "Industry Events" },
+    { src: "/carousel/workshops.png", text: "Learn Together" },
+    { src: "/carousel/together.jpg", text: "Develop Together" },
+    { src: "/carousel/projects.png", text: "Our Projects" },
+    { src: "/projects/notangles/thumbnail.png", text: "Notangles" },
+    { src: "/projects/freerooms/thumbnail.png", text: "Freerooms" },
+    { src: "/projects/unilectives/thumbnail.png", text: "Unilectives" },
+    { src: "/projects/circles/thumbnail.png", text: "Circles" },
+    { src: "/projects/structs/thumbnail.png", text: "Structs.sh" },
+    { src: "/carousel/applications.png", text: "Join us!" },
+  ];
 
   const {
     prevBtnDisabled,
     nextBtnDisabled,
     onPrevButtonClick,
-    onNextButtonClick
-  } = usePrevNextButtons(emblaApi)
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
 
   return (
     <div>
       <div className="embla__controls">
         <div className="embla__buttons">
-            <PrevButton onClick={onPrevButtonClick} />
-            <NextButton onClick={onNextButtonClick} />
-          </div>
+          <PrevButton onClick={onPrevButtonClick} />
+          <NextButton onClick={onNextButtonClick} />
+        </div>
       </div>
       <section className="embla" ref={emblaRef}>
-          <div className="embla__container">
-            {slides.map((e, index) => (
-              <div className="embla__slide" key={index}>
-                  {e.src ? <img className="embla__image" src={e.src}/> : null}
-                  <div className="embla__text">{e.text}</div>
-              </div>
-            ))}
-          </div>
+        <Box
+          className="embla__container"
+          sx={{
+            height: { xs: "200px", sm: "300px", md: "500px" },
+          }}
+        >
+          {slides.map((e, index) => (
+            <div className="embla__slide" key={index}>
+              {e.src ? <img className="embla__image" src={e.src} /> : null}
+              <Typography
+                className="embla__text"
+                sx={{
+                  color: "#EDEEF0",
+                  fontSize: { xs: "1.5rem", sm: "3rem", md: "4rem" },
+                  fontWeight: 500,
+                }}
+              >
+                {e.text}
+              </Typography>
+            </div>
+          ))}
+        </Box>
       </section>
     </div>
-  )
+  );
 }
