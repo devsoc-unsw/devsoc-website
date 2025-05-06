d182065d83b042bb4bb8cb26a9283a1fbc8f0446
Vanessa Bao-Rong Huang <z5476494@ad.unsw.edu.au>
2025-05-06 22:17:02 +1000

    Bolded deadlined and showcase

--- data.ts
+++ data.ts
@@ -634,8 +634,8 @@ export const eventsData: { [year: number]: { [key: string]: EventsData[] } } = {
       url: "/events/2025/t2/starlight.jpg",
       blurb: `🌟 APPLICATIONS FOR STARLIGHT ARE OPEN! 🌟\n
 Ever wanted a platform to showcase your latest project? Then Starlight is the perfect opportunity for YOU!🫵 Starlight is an open gallery event running in Week 8 of Term 2, where developers of all experience levels can showcase their projects and connect with their peers. Find more details in our FB event page!\n
-📅 Deadline: 14th July 2024 11:59pm (Week 7 Monday)
-💫 Showcase: Term 2, Week 8 Day TBC
+📅 𝗗𝗘𝗔𝗗𝗟𝗜𝗡𝗘: 14th July 2024 11:59pm (Week 7 Monday)
+💫 𝗦𝗛𝗢𝗪𝗖𝗔𝗦𝗘: Term 2, Week 8 Day TBC
 📍 𝗟𝗢𝗖𝗔𝗧𝗜𝗢𝗡: TBD`
     }
     ],
