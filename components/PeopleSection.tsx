120b6b771478d88a0005672632022e13631b3ee3
Dylan Huynh <redman29h@gmail.com>
2025-11-12 14:13:33 +1100

    chore: change images and fixup empty fragment

--- components/PeopleSection.tsx
+++ components/PeopleSection.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
-import {PersonProps, teamData} from "../teamData";
-import React, {useEffect, useState} from "react";
+import { PersonProps, teamData } from "../teamData";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import {
   List,
@@ -16,28 +16,32 @@ import {
   Typography,
   ToggleButtonGroup,
   Button,
-  Slider
+  Slider,
 } from "@mui/joy";
 
 export const PeopleSection = () => {
   const [teamYear, setTeamYear] = useState(2025);
-  const [subcommittee, setSubcommittee] = useState(teamData[teamYear]["subcommittees"][0]?.name);
+  const [subcommittee, setSubcommittee] = useState(
+    teamData[teamYear]["subcommittees"][0]?.name
+  );
   useEffect(() => {
-    if (!teamData[teamYear]["subcommittees"].some(s => s.name === subcommittee)){
+    if (
+      !teamData[teamYear]["subcommittees"].some((s) => s.name === subcommittee)
+    ) {
       setSubcommittee(teamData[teamYear]["subcommittees"][0]?.name);
     }
   }, [teamYear]);
 
   const handleTeamYearChange = (
     event: any,
-    newValue: React.SetStateAction<number>,
+    newValue: React.SetStateAction<number>
   ) => {
     if (newValue) {
       setTeamYear(newValue);
     }
   };
 
-  const years = [2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018]
+  const years = [2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018];
 
   function valueText(value: number) {
     return value.toString();
@@ -51,7 +55,7 @@ export const PeopleSection = () => {
         step={1}
         min={2018}
         max={2025}
-        marks={years.map(year => ({value: year, label: `${year}`}))}
+        marks={years.map((year) => ({ value: year, label: `${year}` }))}
         color="devsoc_red"
         onChange={(e, year) => setTeamYear(!Array.isArray(year) ? year : 2025)}
       />
@@ -60,37 +64,61 @@ export const PeopleSection = () => {
         {teamYear == 2023 ? "CSESoc Development Executives" : null}
         {teamYear <= 2022 ? "CSESoc Development Directors" : null}
       </Typography>
-      <Stack direction='row' columnGap={2} rowGap={3} flexWrap="wrap" justifyContent="center">
-        {teamData[teamYear].executives.map((props) => <Person {...props} key={props.name}/>)}
+      <Stack
+        direction="row"
+        columnGap={2}
+        rowGap={3}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {teamData[teamYear].executives.map((props) => (
+          <Person {...props} key={props.name} />
+        ))}
       </Stack>
-      {
-        subcommittee ?
-          <Typography level="h2" pt={4} pb={1} px={4}>
-            Teams
-          </Typography>
-          : null
-      }
-      {
-        teamData[teamYear]["subcommittees"].length > 0 ? <>
-          <Box mx={4} mb={2} pt={1} sx={{overflowX:"scroll", scrollbarWidth: "thin", scrollbarColor: "#232323 transparent"}}>
-            <ToggleButtonGroup aria-label="outlined primary button group" buttonFlex={1} value={subcommittee} onChange={(event, newValue)=> {
-              newValue ? setSubcommittee(newValue) : null
-            }}>
-              {
-                teamData[teamYear]["subcommittees"].map((s) => {
-                  return <Button value={s.name} key={s.name}>{s.name}</Button>
-                })
-              }
+      {subcommittee ? (
+        <Typography level="h2" pt={4} pb={1} px={4}>
+          Teams
+        </Typography>
+      ) : null}
+      {teamData[teamYear]["subcommittees"].length > 0 ? (
+        <>
+          <Box
+            mx={4}
+            mb={2}
+            pt={1}
+            sx={{
+              overflowX: "scroll",
+              scrollbarWidth: "thin",
+              scrollbarColor: "#232323 transparent",
+            }}
+          >
+            <ToggleButtonGroup
+              aria-label="outlined primary button group"
+              buttonFlex={1}
+              value={subcommittee}
+              onChange={(event, newValue) => {
+                newValue ? setSubcommittee(newValue) : null;
+              }}
+            >
+              {teamData[teamYear]["subcommittees"].map((s) => {
+                return (
+                  <Button value={s.name} key={s.name}>
+                    {s.name}
+                  </Button>
+                );
+              })}
             </ToggleButtonGroup>
           </Box>
-          {
-            teamData[teamYear].subcommittees.filter(s => s.name == subcommittee).map((props) => <SubcommitteeList {...props} key={props.name}/>)
-          }
-        </> : null
-      }
+          {teamData[teamYear].subcommittees
+            .filter((s) => s.name == subcommittee)
+            .map((props) => (
+              <SubcommitteeList {...props} key={props.name} />
+            ))}
+        </>
+      ) : null}
     </>
-  )
-}
+  );
+};
 
 export interface ExecProps {
   name: string;
@@ -101,79 +129,89 @@ export interface ExecProps {
 const Person: React.FC<ExecProps> = ({ name, title, imgUrl }) => {
   return (
     <Stack
-      direction='column'
-      alignItems='center'
+      direction="column"
+      alignItems="center"
       width={{ xs: "100%", sm: "30%", md: "23%" }}
       textAlign="center"
     >
-      <AspectRatio ratio="1" sx={{ width: { xs: "50%", sm: "80%"}, borderRadius: "50%", mb: 1 }}>
-        {imgUrl
-          ? (
+      <AspectRatio
+        ratio="1"
+        sx={{ width: { xs: "50%", sm: "80%" }, borderRadius: "50%", mb: 1 }}
+      >
+        {imgUrl ? (
+          <Image fill alt={`Photo of ${name}`} src={imgUrl} />
+        ) : (
+          <Skeleton variant="overlay">
             <Image
               fill
-              alt={`Photo of ${name}`}
-              src={imgUrl}
+              alt={`Placeholder for photo of ${name}`}
+              src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
             />
-          ) : (
-            <Skeleton variant="overlay">
-              <Image
-                fill
-                alt={`Placeholder for photo of ${name}`}
-                src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-              />
-            </Skeleton>
-          )
-        }
+          </Skeleton>
+        )}
       </AspectRatio>
-      <Typography level="title-lg">
-        {name}
-      </Typography>
-      <Typography>
-        {title}
-      </Typography>
+      <Typography level="title-lg">{name}</Typography>
+      <Typography>{title}</Typography>
     </Stack>
-  )
+  );
 };
 
-const SubcommitteeList: React.FC<{ name: string, directors: PersonProps[], subcommittee: PersonProps[], other?: {role: string, people: PersonProps[]}[] }> = ({name: string, directors, subcommittee, other}) => {
+const SubcommitteeList: React.FC<{
+  name: string;
+  directors: PersonProps[];
+  subcommittee: PersonProps[];
+  other?: { role: string; people: PersonProps[] }[];
+}> = ({ name: string, directors, subcommittee, other }) => {
   return (
     <>
       <Box px={3}>
         <List
           sx={{
-            '--ListItemDecorator-size': '56px',
-            display: 'grid',
+            "--ListItemDecorator-size": "56px",
+            display: "grid",
             gridTemplateColumns: {
-              xs: '1fr',
-              sm: 'repeat(2, 1fr)'
+              xs: "1fr",
+              sm: "repeat(2, 1fr)",
             },
             gap: 1,
           }}
         >
-          {
-            directors.map((props) => {
-              return (
-                <TeamListItem name={props.name} imgUrl={props.imgUrl} title="Director" key={props.name}/>
-              )
-            })
-          }
-          {
-            subcommittee.map((props) => {
-              return (
-                <TeamListItem name={props.name} imgUrl={props.imgUrl} title="Subcommittee" key={props.name}/>
-              )
-            })
-          }
-          {
-            other ? other.map(({role, people}) => {
-              return people.map((props) => <TeamListItem name={props.name} imgUrl={props.imgUrl} title={role} key={props.name}/>);
-            }): <></>
-          }
+          {directors.map((props) => {
+            return (
+              <TeamListItem
+                name={props.name}
+                imgUrl={props.imgUrl}
+                title="Director"
+                key={props.name}
+              />
+            );
+          })}
+          {subcommittee.map((props) => {
+            return (
+              <TeamListItem
+                name={props.name}
+                imgUrl={props.imgUrl}
+                title="Subcommittee"
+                key={props.name}
+              />
+            );
+          })}
+          {other &&
+            other.map(({ role, people }) => {
+              return people.map((props) => (
+                <TeamListItem
+                  name={props.name}
+                  imgUrl={props.imgUrl}
+                  title={role}
+                  key={props.name}
+                />
+              ));
+            })}
         </List>
       </Box>
     </>
-  )
-}
+  );
+};
 
 interface TeamListItemProps {
   name: string;
@@ -193,5 +231,5 @@ const TeamListItem = (props: TeamListItemProps) => {
         </Typography>
       </ListItemContent>
     </ListItem>
-  )
-}
+  );
+};
